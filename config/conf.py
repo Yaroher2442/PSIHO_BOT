c4d358695d9b28e56f619ce19f7bc4c9b5111242
akadyrova <albinakad8@yandex.ru>
2021-07-23 22:30:01 +0300

    log configs added

--- config/conf.py
+++ config/conf.py
@@ -8,7 +8,7 @@ class Configurator():
             self.json_conf = dict(json.load(conf_file))
         self.tg_conf = TG_config(**self.json_conf['telegram'])
         self.db_conf = DB_config(**self.json_conf['database'])
-
+        self.log_conf = LOG_config(**self.json_conf["logging"])
 
 class TG_config:
     def __init__(self, **kwargs):
@@ -27,6 +27,12 @@ class DB_config:
         for k, v in kwargs.items():
             setattr(self, k, v)
 
+class LOG_config:
+    def __init__(self, **kwargs):
+        self.when = None
+        self.interval = None
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
 if __name__ == '__main__':
     conf = Configurator()
--- loger.py
+++ loger.py
@@ -1,12 +1,18 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
+from config.conf import Configurator
+
+TRFH = Configurator()
+
 
 class log_creator(logging.Logger):
-    def __init__(self, loger_name,log_file):
+    def __init__(self, loger_name, log_file):
         logging.Logger.__init__(self, name=loger_name)
         self.stream_handler = logging.StreamHandler()
         self.stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.rotate = TimedRotatingFileHandler(log_file, when=TRFH.log_conf.when,
+                                               interval=TRFH.log_conf.interval)  # макс размер файла и макс кол-во хранимых файлов
+        self.rotate.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
         self.handlers.append(self.stream_handler)
-        self.handler = logging.FileHandler(log_file)
-        self.format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        self.handler.setFormatter(self.format)
-        self.handlers.append(self.handler)
+        self.handlers.append(self.rotate)
