ad251704e9a5ca0364397b3da4279133bfd19531
Yaroslav <47113131+Yaroher2442@users.noreply.github.com>
2021-08-02 12:08:53 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 echo "[+] Creating virtual environment"
 python3.8 -m venv venv
+echo "[+] Update pip"
+python3.8 -m pip install --upgrade pip
 echo "[+] Activating virtual environment"
 source "$PWD"/venv/bin/activate
 echo "[+] Installing packeges"
