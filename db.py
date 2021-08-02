efeebb588d95319533dbfe679068f3d457bcbb54
Yaroslav <47113131+Yaroher2442@users.noreply.github.com>
2021-08-02 16:19:36 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 from database import DB_interface
+from database import models
 
 if __name__ == '__main__':
-    DB_interface.create_db()
+    # DB_interface.create_db()
+    DB_interface.create_one_table(models.AnswersStatistic)
